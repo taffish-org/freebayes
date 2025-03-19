c36694681ae9684bb767dff01f28a9df05fdbb22
Kaiyuan Han <hermit_han@163.com>
2025-03-20 03:22:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-freebayes
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/freebayes/freebayes
+
+Bayesian haplotype-based genetic polymorphism discovery and genotyping.
---
+++ freebayes.info.lisp
@@ -0,0 +1,16 @@
+(:name           "freebayes")
+(:repo           "taffish-org/freebayes")
+(:author         "KaiyuanHan")
+(:version        "1.3.9")
+(:intro          "Bayesian haplotype-based genetic polymorphism discovery and genotyping.")
+(:dependencies)
+(:functions      "freebayes"
+                 "bamleftalign"
+                 "python   :: v3.11.2"
+                 "bc       :: v1.07.1"
+                 "samtools :: v1.21"
+                 "bcftools :: v1.21"
+                 "parallel :: v20221122"
+                 "meson    :: v1.0.1"
+                 "ninja    :: v1.11.1"
+                 "vcftools :: v0.1.16")
